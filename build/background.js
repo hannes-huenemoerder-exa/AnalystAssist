3f13c0a3a0914132472cc3fa26ba92808c632dfe
hannes-huenemoerder-exa <hannes@exabeam.com>
2023-06-07 13:57:16 +0200

    initial comit

---
+++ build/background.js
@@ -0,0 +1,29 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	/************************************************************************/
+var __webpack_exports__ = {};
+/*!***************************!*\
+  !*** ./src/background.js ***!
+  \***************************/
+
+
+// With background scripts you can communicate extension files.
+// For more information on background script,
+// See https://developer.chrome.com/extensions/background_pages
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.type === 'GREETINGS') {
+    const message = `Hi Ove, my name is Bac. I am from Background. It's great to hear from you.`;
+
+    // Log message coming from the `request` parameter
+    console.log(request.payload.message);
+    // Send a response message
+    sendResponse({
+      message,
+    });
+  }
+});
+
+/******/ })()
+;
+//# sourceMappingURL=background.js.map
